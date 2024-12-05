f26ad182718b5d555b06c6ff0fe32e836f3bfc6e
Tajammal Husnain <tajammalhusnain1996@gmail.com>
2024-12-05 11:56:10 +0500

    Fixes applied

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,11 +3,10 @@ import PostsController from './posts.controller';
 import PostsService from './posts.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Post from './entities/post.entity';
-import { ElasticsearchService } from '@nestjs/elasticsearch';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post])],
   controllers: [PostsController],
-  providers: [PostsService, ElasticsearchService],
+  providers: [PostsService],
 })
 export class PostsModule {}
